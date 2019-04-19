baa37be583b536a8da4b23935ee5e8cd855340ee
zackJKnight <zachary.j.knight@gmail.com>
2019-04-18 21:06:56 -0500

    Learning how to use Angular and Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PlayerAddComponent } from './player/player-add/player-add.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { PlayerAddComponent } from "./player/player-add/player-add.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 @NgModule({
-  declarations: [
-    AppComponent,
-    PlayerAddComponent
-  ],
+  declarations: [AppComponent, PlayerAddComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule
+    FormsModule,
+    MatFormFieldModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/player/player-add/player-add.component.ts
+++ src/app/player/player-add/player-add.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Position } from '../../position';
 import { POSITIONS } from '../../positions';
-
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-player-add',
   templateUrl: './player-add.component.html',
   styleUrls: ['./player-add.component.less']
 })
 export class PlayerAddComponent implements OnInit {
-
+  positions = POSITIONS;
   constructor() { }
 
   ngOnInit() {
   }
-positions = POSITIONS;
+
+drop(event: CdkDragDrop<Position[]>) {
+  moveItemInArray(this.positions, event.previousIndex, event.currentIndex);
+}
 }
